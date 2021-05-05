7141b7710bc1c1a403a2ae0d0a9767785160041b
kc-hrt <kaseyh@pm.me>
2021-05-04 23:54:54 -0500

    upload

--- index.js
+++ index.js
@@ -14,12 +14,24 @@ const displayDate = () => {
 }
  
 
+
 // Write a JavaScript program to convert a number to a string.
+const numString = (num) => {
+  return num.toString();
+}
+
+console.log(numString(7) + "   type: " + typeof(numString(7)));
+console.log(" ");
 
 
 
 // Write a JavaScript program to convert a string to the number.
+const stringNum = (string) => {
+  return parseInt(string);
+}
 
+console.log(stringNum("77") + "  type: " + typeof(stringNum("77")));
+console.log(" ");
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
@@ -29,22 +41,62 @@ const displayDate = () => {
   // * Number
   // * NaN
   // * String
-  
+  const varType = (variable) => {
+    return typeof(variable);
+  }
 
+  console.log(varType(true));
+  console.log(varType(null));
+  console.log(varType());
+  console.log(varType(777));
+  console.log(varType("cat"/4));
+  console.log(varType("dog"));
+  console.log(" ");
   
 // Write a JavaScript program that adds 2 numbers together.
+const addingTwo = (add0, add1) => {
+  return add0 + add1;
+}
 
-
+console.log(addingTwo(7,3));
+console.log(" ");
 
 // Write a JavaScript program that runs only when 2 things are true.
-
+const twoTruths = (var1, var2) => {
+  if (var1 == 7 && var2 == 7) {
+    return true;
+  }
+}
+console.log(twoTruths(7,7));
+console.log(twoTruths(7,3));
+console.log(" ");
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
-
+const andALie = (var1, var2) => {
+  if (var1 == 7 || var2 == 7) {
+    return true;
+  }
+}
+console.log(andALie(7,7));
+console.log(andALie(7,3));
+console.log(andALie(3,3));
+console.log(" ");
 
 
 // Write a JavaScript program that runs when both things are not true.  
+const nothingMatters = (var1, var2) => {
+  if (var1 == 7 && var2 == 7) {
+    // return true;
+  } else {
+      return "false=true";
+    }
+}
+console.log(nothingMatters(7,7));
+console.log(nothingMatters(7,3));
+console.log(" ");
+
+
 
 // ***************************
 //         PART TWO
